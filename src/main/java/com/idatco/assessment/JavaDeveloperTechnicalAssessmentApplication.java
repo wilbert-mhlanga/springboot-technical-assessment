ca2482ae0979315652d4e7d404bed2bedfe22a8f
R5413168 <Wilbert.Mhlanga@rmb.co.za>
2025-07-24 15:07:25 +0200

    Initial commit

---
+++ src/main/java/com/idatco/assessment/JavaDeveloperTechnicalAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.idatco.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDeveloperTechnicalAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDeveloperTechnicalAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idatco/assessment/JavaDeveloperTechnicalAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idatco.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDeveloperTechnicalAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
